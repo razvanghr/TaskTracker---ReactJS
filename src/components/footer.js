a4876533755ca9873afdc8e4408d6436b67a38ba
Razvan Alin <razvan_alin02@yahoo.com>
2023-05-25 21:01:54 +0300

    task-tracker

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2023</p>
+      <a href="https://github.com/razvanghr">Razvan Ghilea</a>
+    </footer>
+  );
+};
+
+export default Footer;
